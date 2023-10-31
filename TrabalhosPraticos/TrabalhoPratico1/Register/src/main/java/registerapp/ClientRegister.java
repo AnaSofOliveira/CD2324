6f2e14a8a3aae7f512efe444b89eb451fc983db1
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2023-10-31 20:22:21 +0000

    ClientRegister e ServerRegister

--- TrabalhosPraticos/TrabalhoPratico1/Register/src/main/java/registerapp/ClientRegister.java
+++ TrabalhosPraticos/TrabalhoPratico1/Register/src/main/java/registerapp/ClientRegister.java
@@ -1,8 +1,14 @@
 package registerapp;
 
 import cliregstubs.ClientRegisterServiceGrpc;
+import cliregstubs.Result;
+import cliregstubs.Void;
+import io.grpc.stub.StreamObserver;
 
 public class ClientRegister extends ClientRegisterServiceGrpc.ClientRegisterServiceImplBase {
 
-
+    @Override
+    public void getServer(Void request, StreamObserver<Result> responseObserver) {
+        throw new java.lang.UnsupportedOperationException("Not implemented.");
+    }
 }
--- TrabalhosPraticos/TrabalhoPratico1/Register/src/main/java/registerapp/ServerRegister.java
+++ TrabalhosPraticos/TrabalhoPratico1/Register/src/main/java/registerapp/ServerRegister.java
@@ -1,6 +1,14 @@
 package registerapp;
 
+import io.grpc.stub.StreamObserver;
+import servregstubs.Reply;
+import servregstubs.Result;
 import servregstubs.ServerRegisterServiceGrpc;
 
 public class ServerRegister extends ServerRegisterServiceGrpc.ServerRegisterServiceImplBase {
+
+    @Override
+    public void registerServer(Reply request, StreamObserver<Result> responseObserver) {
+        throw new java.lang.UnsupportedOperationException("Not implemented.");
+    }
 }
